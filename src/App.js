acebac857fbbab241a46da980b19ebf4dea0d4bb
harshalborkar <harshalborkar696@gmail.com>
2024-06-22 17:14:53 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,94 +1,93 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-// import Home from "./Components/Home";
+// import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 import Footer from "./Components/Footer";
-// import About from "./Components/About";
-// import CourseCard from "./Components/CourseCard";
-// import CourseContent from "./Components/CourseContent";
+import About from "./Components/About";
+import CourseCard from "./Components/CourseCard";
+import CourseContent from "./Components/CourseContent";
 
 function App() {
-  // const courses = [
-  //   {
-  //     title: "Programming Languages",
-  //     id: "programming",
-  //     description: "All programming language courses",
-  //     modules: [
-  //       { name: "C Programming", pathlink: "/cprogramming" },
-  //       { name: "C++ Programming", pathlink: "/cpp" },
-  //       { name: "Python Programming", pathlink: "/python" },
-  //       { name: "Java Programming", pathlink: "/java" },
-  //       { name: "JavaScript Programming", pathlink: "/javascript" },
-  //     ],
-  //   },
-  //   {
-  //     title: "Domain Specific",
-  //     id: "domainspecific",
-  //     description: "All domain specific course",
-  //     modules: [
-  //       { name: "Web Development", pathlink: "/webdevelopment" },
-  //       { name: "App Development", pathlink: "/appdevelopment" },
-  //       { name: "Game Development", pathlink: "/gamedevelopment" },
-  //       { name: "Robotics & IoT", pathlink: "/roboticsiot" },
-  //       { name: "Database Management", pathlink: "/cpp" },
-  //     ],
-  //   },
-  //   {
-  //     title: "Foreign Languages",
-  //     id: "foreign",
-  //     description: "All domain specific course",
-  //     modules: [
-  //       { name: "Spanish" },
-  //       { name: "Chinese" },
-  //       { name: "French" },
-  //       { name: "Italian" },
-  //       { name: "Japanese" },
-  //     ],
-  //   },
-  //   {
-  //     title: "General Skills",
-  //     id: "generalskill",
-  //     description: "All domain specific course",
-  //     modules: [
-  //       { name: "MS-Word" },
-  //       { name: "MS-Excel" },
-  //       { name: "MS-Powerpoint" },
-  //       { name: "Canva" },
-  //       { name: "Photoshop" },
-  //     ],
-  //   },
-  //   {
-  //     title: "21st Century Skills",
-  //     id: "lifeskill",
-  //     description: "All domain specific course",
-  //     modules: [
-  //       { name: "Personality Development" },
-  //       { name: "Communication Skills" },
-  //       { name: "Abacus" },
-  //       { name: "Vedic Maths" },
-  //       { name: "Critical Thinking" },
-  //     ],
-  //   },
-  // ];
-  // const contents=[
-  //   "Basic",
-  //   "Loops & Conditional statements",
-  //   "Functions & Pointers",
-  //   "Object Oriented Programming",
-  //   "Advance Concepts",
-  //   "Final Projects",
-  // ];
+  const courses = [
+    {
+      title: "Programming Languages",
+      id: "programming",
+      description: "All programming language courses",
+      modules: [
+        { name: "C Programming", pathlink: "/cprogramming" },
+        { name: "C++ Programming", pathlink: "/cpp" },
+        { name: "Python Programming", pathlink: "/python" },
+        { name: "Java Programming", pathlink: "/java" },
+        { name: "JavaScript Programming", pathlink: "/javascript" },
+      ],
+    },
+    {
+      title: "Domain Specific",
+      id: "domainspecific",
+      description: "All domain specific course",
+      modules: [
+        { name: "Web Development", pathlink: "/webdevelopment" },
+        { name: "App Development", pathlink: "/appdevelopment" },
+        { name: "Game Development", pathlink: "/gamedevelopment" },
+        { name: "Robotics & IoT", pathlink: "/roboticsiot" },
+        { name: "Database Management", pathlink: "/cpp" },
+      ],
+    },
+    {
+      title: "Foreign Languages",
+      id: "foreign",
+      description: "All domain specific course",
+      modules: [
+        { name: "Spanish" },
+        { name: "Chinese" },
+        { name: "French" },
+        { name: "Italian" },
+        { name: "Japanese" },
+      ],
+    },
+    {
+      title: "General Skills",
+      id: "generalskill",
+      description: "All domain specific course",
+      modules: [
+        { name: "MS-Word" },
+        { name: "MS-Excel" },
+        { name: "MS-Powerpoint" },
+        { name: "Canva" },
+        { name: "Photoshop" },
+      ],
+    },
+    {
+      title: "21st Century Skills",
+      id: "lifeskill",
+      description: "All domain specific course",
+      modules: [
+        { name: "Personality Development" },
+        { name: "Communication Skills" },
+        { name: "Abacus" },
+        { name: "Vedic Maths" },
+        { name: "Critical Thinking" },
+      ],
+    },
+  ];
+  const contents=[
+    "Basic",
+    "Loops & Conditional statements",
+    "Functions & Pointers",
+    "Object Oriented Programming",
+    "Advance Concepts",
+    "Final Projects",
+  ];
 
-  // const programmingCatalog = [courses[0]];
-  // const domainCatalog = [courses[1]];
-  // const foreignCatalog = [courses[2]];
-  // const generalCatalog = [courses[3]];
-  // const lifeCatalog = [courses[4]];
+  const programmingCatalog = [courses[0]];
+  const domainCatalog = [courses[1]];
+  const foreignCatalog = [courses[2]];
+  const generalCatalog = [courses[3]];
+  const lifeCatalog = [courses[4]];
   return (
     <BrowserRouter>
-      <Navbar />
-      {/* <main>
+      {/* <Navbar /> */}
+      <main>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/about" element={<About />}></Route>
@@ -137,7 +136,7 @@ function App() {
 
 
         </Routes>
-      </main> */}
+      </main>
       <Footer></Footer>
     </BrowserRouter>
   );
